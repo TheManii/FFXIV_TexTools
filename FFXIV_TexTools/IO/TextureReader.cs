47375b9a5050b8ad262c8e0552581bb4ebbdc8c3
TheManii <TheManiitsnv@gmail.com>
2017-03-06 20:36:25 -0500

    Forgot to update credits in header

--- FFXIV_TexTools/IO/TextureReader.cs
+++ FFXIV_TexTools/IO/TextureReader.cs
@@ -1,5 +1,6 @@
 ﻿// FFXIV TexTools
 // Copyright © 2017 Rafael Gonzalez - All Rights Reserved
+// Copyright © 2017 TheManii, et al. - All Rights Reserved
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
